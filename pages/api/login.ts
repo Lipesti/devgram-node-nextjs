a1114a7daa3c6807c73b847bad6d87482f7bee1d
Felipe Santos <lipes.santos02@gmail.com>
2023-07-28 08:19:53 -0300

    Validação de usuario no banco de dados/ criptografia de senha

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {conectarMongoDb} from '../../middlewares/conectarMongoDb';
-import type {RespostaPadraoMsg} from '../../type/RespostaPadraoMsg'
+import type {RespostaPadraoMsg} from '../../type/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
 
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if (req.method === 'POST') {
         const {login, senha} = req.body;
-        
-        if(login === 'admin@admin.com' && senha === 'Admin@123'){
-           return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+
+        const usuarioEncontrados = await UsuarioModel.find({email : login, senha: md5(senha)});
+        if(usuarioEncontrados && usuarioEncontrados.length > 0) {
+            const usuarioEncontrado = usuarioEncontrados[0];
+           return res.status(200).json({msg : `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`});
         }
         return res.status(400).json({erro : 'Usuário ou senha não encontrado'});
 
